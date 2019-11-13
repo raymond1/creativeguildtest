743774d4af41ce036159c2afed7306daa597de5f
Raymond Leon <raymondleon@raymondleon.ca>
2019-11-13 18:24:20 -0500

    Adds in background gradients. Gitignores public css and js folders.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Album</title>
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+    </head>
+    <body class='bodyGradientLinear'>
+        <div class='bodyGradientRadial'>
+        </div>
     </body>
 </html>
