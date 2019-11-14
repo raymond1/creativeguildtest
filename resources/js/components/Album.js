5bcd51e16787092941f6da960b12c4f8d800a888
Raymond Leon <raymondleon@raymondleon.ca>
2019-11-13 21:42:03 -0500

    Divides the page into top and bottom structures

--- resources/js/components/Album.js
+++ resources/js/components/Album.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Profile from './Profile'
+import Tiles from './Tiles'
 
-export default class Example extends Component {
+export default class Album extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">I'm an example component!</div>
-                        </div>
-                    </div>
+            <div className="mainBackgroundLinear">
+                <div className="mainBackgroundRadial">
+                    <Profile></Profile>
+                    <Tiles></Tiles>
                 </div>
             </div>
         );
     }
+
+    componentDidMount() {
+        fetch("landscapes.json")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    //alert(JSON.stringify(result))
+                    this.setState({
+                        isLoaded: true,
+                        items: result.items
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    /*
+                    this.setState({
+                    isLoaded: true,
+                    error
+                    });*/
+                }
+            )
+    }
 }
 
 if (document.getElementById('root')) {
-    ReactDOM.render(<Example />, document.getElementById('root'));
+    ReactDOM.render(<Album />, document.getElementById('root'));
 }
