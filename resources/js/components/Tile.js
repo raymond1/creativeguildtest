723dc550e86c43d531c461572f3da0d08f691478
Raymond Leon <raymondleon@raymondleon.ca>
2019-11-14 20:39:53 -0500

    Adds description of bottom tile

--- resources/js/components/Tile.js
+++ resources/js/components/Tile.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class Tile extends Component {
     render() {
-        return (<div className="tile"><img src={this.props.img} alt=''></img> {this.props.title}</div>)
+        return (<div className="tile"><img src={this.props.img} alt=''></img> <h2>{this.props.title}</h2>
+        <div className="tileBottom"><p>{this.props.description}</p>
+            <div className="tileHeartDate"><span className="tileDate">{this.props.date}</span></div>
+        </div>
+        </div>)
     }
 }
